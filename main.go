35bf99034d3a1b28d69cacf9a8f7e1de5c8a099d
databaller <databaller53@gmail.com>
2023-06-05 23:34:30 +0530

    endpoints added

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
-func main(){
-	
+func main() {
+
+	// static files will be served from following directory
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	// serve the static files when home is hit by user
+	http.Handle("/", fileServer)
+
+	// when /form endpoint is hit then run formHandler function
+	http.HandleFunc("/form", formHandler)
+
+	// when /hello endpoint is hit then run helloHandler function
+	http.HandlerFunc("/hello", helloHandler)
+
+	// code to be written
+
 }
