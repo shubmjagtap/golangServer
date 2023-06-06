dfa5d0218673331aef996eeec40dcfa64fc7c1c1
shubmjagtap <smjdevopservices@gmail.com>
2023-06-06 23:56:01 +0530

    Added implementation of hello endpoint

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
+
+// function for handling the hello endpoint
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	// if given endpoint is not hello endpoint
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 : path not found", http.StatusNotFound)
+		return
+	}
+
+	// if method applied for hello endpoint is wrong
+	if r.Method != "GET" {
+		http.Error(w, "404 : path not found", http.StatusNotFound)
+		return
+	}
+
+	// if everything is good then print it
+	fmt.Fprint(w, "hello")
+
+}
 
 func main() {
 
@@ -18,6 +40,11 @@ func main() {
 	// when /hello endpoint is hit then run helloHandler function
 	http.HandlerFunc("/hello", helloHandler)
 
-	// code to be written ny shubham
+	// indicate the start of server
+	fmt.Printf("Starting server at port 3000")
 
+	// start the server to listen and server and if there is error then print it
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
